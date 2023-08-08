1f4ceba4aa8f7b0ab26012d5a336e2c5f6a693a6
marryjianjian <logjian@gmail.com>
2023-08-08 11:06:02 +0800

    feat: add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/vimrc.plugs ~/.vimrc.plugs
+
+mkdir -p ~/.config/nvim
+ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
