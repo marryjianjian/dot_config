8cef5d9421552f4aa556a99cb88e73594c7043e9
marryjianjian <logjian@gmail.com>
2023-08-24 16:38:37 +0800

    feat: copy ragel and thraxgrm vim file to ~/.vim/syntax

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/vimrc.plugs ~/.vimrc.plugs
 
+mkdir -p ~/.vim/syntax
+cp $(pwd)/ragel.vim $(pwd)/thraxgrm.vim ~/.vim/syntax
 mkdir -p ~/.config/nvim
 ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
